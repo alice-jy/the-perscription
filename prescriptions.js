4c3780fff7db21682709d98010dd582fdbd09581
Lester Lee <contact@lester-lee.com>
2023-05-19 16:28:06 -0400

    Add more documentation

--- prescriptions.js
+++ prescriptions.js
@@ -1,3 +1,4 @@
+// Sample customer data
 const timmy = {
   prescription: "acetaminophen",
   pricePerRefill: 25,
@@ -22,33 +23,42 @@ const rocky = {
   coupon: true,
 };
 
-// Function to find the initial cost of all refills.
-function totalCost(pricePerRefill, refills) {
+/**
+ * Calculates the total cost of all refills.
+ * @param {number} pricePerRefill
+ * @param {number} refills
+ * @returns {number} total cost of all refills
+ */
+function getTotalCost(pricePerRefill, refills) {
   return pricePerRefill * refills;
 }
 
-// Apply the subscription discount of 25% to the total cost of the refills.
-function withDiscount(totalCostWithoutDiscounts, subscription) {
-  if (subscription) {
-    return totalCostWithoutDiscounts * 0.75;
-  } else {
-    return totalCostWithoutDiscounts;
-  }
+/**
+ * If the customer has a subscription, apply a 25% discount
+ * to the total cost of the refills.
+ * @param {number} totalCostWithoutDiscounts
+ * @param {boolean} isSubscribed whether the user has a subscription
+ * @returns {number} total cost with subscription discount
+ */
+function applyDiscount(totalCostWithoutDiscounts, isSubscribed) {
+  return totalCostWithoutDiscounts * (isSubscribed ? 0.75 : 1);
 }
 
-// Apply the $10 coupon to the cost after the subscription discount.
-function withCoupon(costAfterSubscription, coupon) {
-  if (coupon) {
-    return costAfterSubscription - 10;
-  } else {
-    return costAfterSubscription;
-  }
+/**
+ * If the customer has a coupon, apply a $10 discount to
+ * the total cost of the refills after the subscription discount.
+ * @param {number} costAfterSubscription
+ * @param {boolean} hasCoupon whether the customer is using a coupon
+ * @returns
+ */
+function applyCoupon(costAfterSubscription, hasCoupon) {
+  return costAfterSubscription - (hasCoupon ? 10 : 0);
 }
 
 module.exports = {
-  totalCost,
-  withDiscount,
-  withCoupon,
+  getTotalCost,
+  applyDiscount,
+  applyCoupon,
   timmy,
   sarah,
   rocky,
