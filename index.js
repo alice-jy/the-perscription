71dd5edcb7d04f47116bafa7a4259ab06973ecec
Lester Lee <contact@lester-lee.com>
2023-05-22 10:32:26 -0400

    Move functions into one file for easier exporting

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 /**
+ * Calculates the total cost of all refills.
+ * @param {number} pricePerRefill
+ * @param {number} refills
+ * @returns {number} total cost of all refills
+ */
+function getTotalCost(pricePerRefill, refills) {
+  return pricePerRefill * refills;
+}
+
+/**
+ * If the customer has a subscription, apply a 25% discount
+ * to the total cost of the refills.
+ * @param {number} totalCostWithoutDiscounts
+ * @param {boolean} isSubscribed whether the user has a subscription
+ * @returns {number} total cost with subscription discount
+ */
+function applyDiscount(totalCostWithoutDiscounts, isSubscribed) {
+  return totalCostWithoutDiscounts * (isSubscribed ? 0.75 : 1);
+}
+
+/**
+ * If the customer has a coupon, apply a $10 discount to
+ * the total cost of the refills after the subscription discount.
+ * @param {number} costAfterSubscription
+ * @param {boolean} hasCoupon whether the customer is using a coupon
+ * @returns
+ */
+function applyCoupon(costAfterSubscription, hasCoupon) {
+  return costAfterSubscription - (hasCoupon ? 10 : 0);
+}
+
+/**
  * Calculates the cost of a subscription based on input values
  * and displays the result on the page.
  */
@@ -22,6 +54,9 @@ function calculateCost() {
 
 if (typeof module !== "undefined") {
   module.exports = {
+    getTotalCost,
+    applyDiscount,
+    applyCoupon,
     calculateCost,
   };
 } else {
