87f603aced3d94976505933b0660224bdbf70bc3
Lester Lee <contact@lester-lee.com>
2023-05-22 10:32:40 -0400

    Update starter test files

--- __tests__/prescriptions.test.js
+++ __tests__/prescriptions.test.js
@@ -1,9 +1,7 @@
-const {
-  getTotalCost,
-  applyDiscount,
-  applyCoupon,
-} = require("../prescriptions.js");
+const { getTotalCost, applyDiscount, applyCoupon } = require("../index.js");
 
+// NOTE: This array illustrates the shape of the data that
+// will be used in the functions you are testing.
 const sampleData = [
   {
     prescription: "acetaminophen",
@@ -28,7 +26,9 @@ const sampleData = [
   },
 ];
 
-// This is already complete - use this as reference!
+// NOTE: You do not need to modify this describe block; it is provided for you.
+// These tests are not exhaustive, but they should be enough to give you
+// an idea of what you might want to test, and how to do so.
 describe("getTotalCost()", () => {
   describe("handles expected input", () => {
     it("returns the correct cost for a single refill", () => {
@@ -43,6 +43,13 @@ describe("getTotalCost()", () => {
     it("returns the correct cost for zero refills", () => {
       expect(getTotalCost(25, 0)).toBe(0);
     });
+    it("returns the correct cost for random numbers", () => {
+      const pricePerRefill = Math.floor(Math.random() * 100);
+      const refills = Math.floor(Math.random() * 10);
+      const result = getTotalCost(pricePerRefill, refills);
+      const expected = pricePerRefill * refills;
+      expect(result).toBe(expected);
+    });
     it("returns the correct costs for the sample data", () => {
       sampleData.forEach((data) => {
         const result = getTotalCost(data.pricePerRefill, data.refills);
@@ -54,6 +61,11 @@ describe("getTotalCost()", () => {
 
   // NOTE: These tests will fail - why?
   describe.skip("handles unexpected input", () => {
+    it("returns 0 if either argument is not a number", () => {
+      expect(getTotalCost("25", 3)).toBe(0);
+      expect(getTotalCost(25, "3")).toBe(0);
+      expect(getTotalCost("25", "3")).toBe(0);
+    });
     it("returns 0 if pricePerRefill is negative", () => {
       expect(getTotalCost(-25, 3)).toBe(0);
     });
